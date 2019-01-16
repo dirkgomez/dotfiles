a11513fb03f0b80127a2a5bb08041e559ab11577
Dirk Gómez <dirk@dirkgomez.de>
2019-01-16 23:28:13 +0100

    Add FZF for Mac OS X

--- Install.sh
+++ Install.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew install fzf
+  VIMRC_FZF="set runtimepath+=/usr/local/opt/fzf"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Linux"
+fi
+
+
 cat << EOF > ~/vimrc
 set runtimepath+=~/dotfiles/vim
 set packpath+=~/dotfiles/vim/packages
 source ~/dotfiles/vim/vimrc.mine
+$VIMRC_FZF
 EOF
+
 mv ~/vimrc ~/.vimrc
+
