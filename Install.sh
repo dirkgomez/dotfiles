c0519fbd0e9fb865e9a682fe40a626a2cf9ea97b
Dirk Gómez <dirk@dirkgomez.de>
2019-06-08 11:14:58 +0200

    python-mode -> coc

--- Install.sh
+++ Install.sh
@@ -26,32 +26,33 @@ function cat_to_file_if_not_exists {
   cat "$SOURCE" >> "$TARGET"
 }
 
+if [ "$(uname)" == "Darwin" ]; then
+  BASH_CFG="$HOME/.bash_profile"
+  brew install ctags yarn node
+elif [ "$(awk -F= '/^NAME/{print $2}' /etc/os-release)" == "\"Ubuntu\"" ]; then
+  BASH_CFG="$HOME/.bashrc"
+  apt-get install ctags
+fi
+
 # create packages directory
 START_PLUGINS_DIR=~/.vim/pack/plugins/start
 mkdir -p START_PLUGINS_DIR
 
 git_clone_or_pull https://github.com/sheerun/vim-polyglot ${START_PLUGINS_DIR}/vim-polyglot
 git_clone_or_pull https://github.com/python-mode/python-mode.git ${START_PLUGINS_DIR}/python-mode
-(cd ${START_PLUGINS_DIR}/python-mode && git submodule update --init --recursive)
+#(cd ${START_PLUGINS_DIR}/python-mode && git submodule update --init --recursive)
 git_clone_or_pull https://github.com/mileszs/ack.vim.git ${START_PLUGINS_DIR}/ack
 git_clone_or_pull https://github.com/tpope/commentary.git ${START_PLUGINS_DIR}/commentary
 git_clone_or_pull https://github.com/tpope/surround.git ${START_PLUGINS_DIR}/surround
 git_clone_or_pull https://github.com/tpope/vim-fugitive.git ${START_PLUGINS_DIR}/vim-fugitive
-git_clone_or_pull https://github.com/w0rp/ale.git ${START_PLUGINS_DIR}/ale
 git_clone_or_pull https://github.com/morhetz/gruvbox.git ${START_PLUGINS_DIR}/gruvbox
 git_clone_or_pull https://github.com/MarcWeber/vim-addon-mw-utils ${START_PLUGINS_DIR}/vim-addon-mw-utils
 git_clone_or_pull https://github.com/chriskempson/base16-vim ${START_PLUGINS_DIR}/base16-vim
 git_clone_or_pull https://tpope.io/vim/projectionist.git ${START_PLUGINS_DIR}/projectionist
 git_clone_or_pull https://github.com/ctrlpvim/ctrlp.vim ${START_PLUGINS_DIR}/ctrlp
 git_clone_or_pull https://github.com/ludovicchabant/vim-gutentags ${START_PLUGINS_DIR}/vim-gutentags
-
-if [ "$(uname)" == "Darwin" ]; then
-  BASH_CFG="$HOME/.bash_profile"
-  brew install ctags
-elif [ "$(awk -F= '/^NAME/{print $2}' /etc/os-release)" == "\"Ubuntu\"" ]; then
-  BASH_CFG="$HOME/.bashrc"
-  apt-get install ctags
-fi
+git_clone_or_pull https://github.com/neoclide/coc.nvim ${START_PLUGINS_DIR}/coc.nvim
+(cd ${START_PLUGINS_DIR}/coc.nvim && ./install.sh && yarn install --frozen-lockfile)
 
 echo_to_file_if_not_exists "\"dotfiles/bash.mine\"" $BASH_CFG "source $HOME/dotfiles/bash.mine"
 
@@ -61,3 +62,6 @@ cat << EOF > $HOME/.vimrc
 source ~/dotfiles/vim/vimrc.mine
 $VIMRC_FZF
 EOF
+
+pip3 install python-language-server
+vim +'CocInstall coc-python coc-json' +qall
