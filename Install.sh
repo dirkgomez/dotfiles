5a9f38f4c5780442460a73c5b16f1d7d6962a907
Dirk Gómez <dirk@dirkgomez.de>
2019-10-21 15:13:25 +0200

    Go back to python-mode

--- Install.sh
+++ Install.sh
@@ -4,9 +4,9 @@ function git_clone_or_pull {
   echo "Cloning $1"
   if [ ! -d $2 ]
     then
-    git clone $3 $1 $2
+    git clone  --recurse-submodules  $3 $1 $2
   else
-    (cd $2 && git pull --rebase && git submodule update --init --recursive)
+    (cd $2 && git pull --rebase; git submodule update --init --recursive)
   fi
 }
 
@@ -28,7 +28,7 @@ function cat_to_file_if_not_exists {
 
 if [ "$(uname)" == "Darwin" ]; then
   BASH_CFG="$HOME/.bash_profile"
-  brew install ctags yarn node
+  brew install ctags
 elif [ "$(awk -F= '/^NAME/{print $2}' /etc/os-release)" == "\"Ubuntu\"" ]; then
   BASH_CFG="$HOME/.bashrc"
   apt-get install ctags
@@ -38,17 +38,13 @@ fi
 START_PLUGINS_DIR=~/.vim/pack/plugins/start
 mkdir -p START_PLUGINS_DIR
 
-git_clone_or_pull https://github.com/sheerun/vim-polyglot ${START_PLUGINS_DIR}/vim-polyglot
-#git_clone_or_pull https://github.com/python-mode/python-mode.git ${START_PLUGINS_DIR}/python-mode
-#(cd ${START_PLUGINS_DIR}/python-mode && git submodule update --init --recursive)
+git_clone_or_pull https://github.com/python-mode/python-mode.git ${START_PLUGINS_DIR}/python-mode
 git_clone_or_pull https://github.com/mileszs/ack.vim.git ${START_PLUGINS_DIR}/ack
 git_clone_or_pull https://github.com/tpope/commentary.git ${START_PLUGINS_DIR}/commentary
-git_clone_or_pull https://github.com/tpope/surround.git ${START_PLUGINS_DIR}/surround
-git_clone_or_pull https://github.com/tpope/vim-fugitive.git ${START_PLUGINS_DIR}/vim-fugitive
 git_clone_or_pull https://github.com/morhetz/gruvbox.git ${START_PLUGINS_DIR}/gruvbox
-git_clone_or_pull https://github.com/MarcWeber/vim-addon-mw-utils ${START_PLUGINS_DIR}/vim-addon-mw-utils
 git_clone_or_pull https://github.com/chriskempson/base16-vim ${START_PLUGINS_DIR}/base16-vim
 git_clone_or_pull https://github.com/ctrlpvim/ctrlp.vim ${START_PLUGINS_DIR}/ctrlp
+git_clone_or_pull https://github.com/davidhalter/jedi-vim.git ${START_PLUGINS_DIR}/jedi.vim
 
 echo_to_file_if_not_exists "\"dotfiles/bash.mine\"" $BASH_CFG "source $HOME/dotfiles/bash.mine"
 
@@ -57,7 +53,3 @@ cat_to_file_if_not_exists '\[alias\]' "$HOME/.gitconfig" "$HOME/dotfiles/gitconf
 cat << EOF > $HOME/.vimrc
 source ~/dotfiles/vim/vimrc.mine
 EOF
-
-pip3 install python-language-server
-vim +'CocInstall coc-python coc-json' +qall
-vim -c "helptags ~/.vim/pack/plugins/start/coc.nvim"
