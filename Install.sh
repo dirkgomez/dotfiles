1cbf78f809c030043d775369ad32d8a32e46856a
Dirk Gómez <dirk@dirkgomez.de>
2021-03-27 19:59:42 +0100

    Remove oh-my-zsh

--- Install.sh
+++ Install.sh
@@ -6,13 +6,9 @@ elif [ "$(awk -F= '/^NAME/{print $2}' /etc/os-release)" == "\"Ubuntu\"" ]; then
   apt-get install ctags
 fi
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
-fi
-
-cp dirk.zsh ~/.oh-my-zsh/custom/
 cp gitconfig-aliases ~/.gitconfig
-[[ -f "dirk-nocheckin.zsh" ]] && cp dirk-nocheckin.zsh ~/.oh-my-zsh/custom/
+cp zshrc ~/.zshrc
+cp dirk.sh ~/.zshrc.dirk
 
 git config --global user.email "dirk@dirkgomez.de"
 
