30ca05cdc262f0e7125c22bfe317b66c40719271
Dirk Gómez <dirk@dirkgomez.de>
2021-08-09 13:21:05 +0200

    Go back to oh my zsh

--- CommonInstall.sh
+++ CommonInstall.sh
@@ -1,7 +1,10 @@
 cp gitconfig ~/.gitconfig
 cp gitignore_global ~/.gitignore_global
-cp zshrc ~/.zshrc
-cp zshrc.dirk ~/.zshrc.dirk
+
+[ ! -d "/path/to/dir" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+[ -e zshrc.local-nocheckin ] && cp zshrc.local-nocheckin ~/.oh-my-zsh/custom/zshrc.local.zsh
+cp zshrc.dirk ~/.oh-my-zsh/custom/zshrc.dirk.zsh
 
 mkdir -p ~/.config/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
