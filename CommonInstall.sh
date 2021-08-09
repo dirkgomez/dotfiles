c266217388aed065775fb9324738f8cf858b289d
Dirk Gómez <dirk@dirkgomez.de>
2021-08-09 13:26:14 +0200

    Do not install zsh multiple times

--- CommonInstall.sh
+++ CommonInstall.sh
@@ -1,7 +1,7 @@
 cp gitconfig ~/.gitconfig
 cp gitignore_global ~/.gitignore_global
 
-[ ! -d "/path/to/dir" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 [ -e zshrc.local-nocheckin ] && cp zshrc.local-nocheckin ~/.oh-my-zsh/custom/zshrc.local.zsh
 cp zshrc.dirk ~/.oh-my-zsh/custom/zshrc.dirk.zsh
