56ed3fff9b730e74c0959f17f1e9ced1626ce500
Dirk Gómez <dirk@dirkgomez.de>
2024-03-31 09:36:28 +0200

    Remove nvim

--- CommonInstall.sh
+++ CommonInstall.sh
@@ -10,8 +10,3 @@ mkdir -p ~/.config/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-rm -f ~/.config/nvim/init.vim; ln -s $PWD/nvim/init.vim ~/.config/nvim/init.vim
-
-# nvim
-npm list -g bash-language-server || npm install -g bash-language-server
-npm list -g pyright || npm install -g pyright
