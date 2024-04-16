f07e15adb92f1c75cb8faa6f002b108e802eebd0
Dirk Gómez <dirk@dirkgomez.de>
2024-04-16 16:52:25 +0200

    Spice up the Mac config

--- CommonInstall.sh
+++ CommonInstall.sh
@@ -1,7 +1,3 @@
 cp gitconfig ~/.gitconfig
 cp gitignore_global ~/.gitignore_global
 
-[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-[ -e zshrc.local-nocheckin ] && cp zshrc.local-nocheckin ~/.oh-my-zsh/custom/zshrc.local.zsh
-cp zshrc.dirk ~/.oh-my-zsh/custom/zshrc.dirk.zsh
--- MacInstall.sh
+++ MacInstall.sh
@@ -1,11 +1,21 @@
+#!/bin/zsh
 brew install \
             asdf \
             coreutils \
             fzf \
             git \
             gron \
+            hugo \
+            nvim \
+            pipx \
             xsv
 
 asdf plugin-add python
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+[ -e zshrc.local-nocheckin ] && cp zshrc.local-nocheckin ~/.oh-my-zsh/custom/zshrc.local.zsh
+cp zshrc.dirk ~/.oh-my-zsh/custom/zshrc.dirk.zsh
 
 . ./CommonInstall.sh
