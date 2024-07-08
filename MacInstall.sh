5a9e5bb53df918e086857d937db4cbd37cc67e05
Dirk Gómez <dirk@dirkgomez.de>
2024-07-08 09:50:42 +0200

    Updates

--- MacInstall.sh
+++ MacInstall.sh
@@ -17,4 +17,11 @@ asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 [ -e zshrc.local-nocheckin ] && cp zshrc.local-nocheckin ~/.oh-my-zsh/custom/zshrc.local.zsh
 cp zshrc.dirk ~/.oh-my-zsh/custom/zshrc.dirk.zsh
 
+asdf global python 3.12.3 && pip install --user jupyterlab \
+ pandas \
+ seaborn \
+ matplotlib \
+ pyarrow \
+ scikit-learn
+
 . ./CommonInstall.sh
