ccf1b5ce61901be0df9a9a3fa07ebf67a4ebbaa8
Dirk Gómez <dirk@dirkgomez.de>
2026-02-27 16:47:39 +0100

    Remove asdf

--- MacInstall.sh
+++ MacInstall.sh
@@ -9,9 +9,6 @@ brew install \
     pipx \
     xsv
 
-asdf plugin-add python
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-
 [ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 [ -e zshrc.local-nocheckin ] && cp zshrc.local-nocheckin ~/.oh-my-zsh/custom/zshrc.local.zsh
